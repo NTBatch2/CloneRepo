0409a8c40a98793d8a3438e9151025347d781c8c
noort <noort@10.1.10.35>
2020-05-16 14:42:01 -0400

    fixed broken code

--- src/noorteck/StashPractice.java
+++ src/noorteck/StashPractice.java
@@ -1,32 +1,30 @@
 package noorteck;
 
 public class StashPractice {
-	public static void getName(){
+	public static void getName() {
 		System.out.println("GithUb Commit");
 	}
-	
+
 	public static void main(String[] args) {
-		//stash....
-<<<<<<< HEAD
-=======
+
 		int a = 10;
-		int b= 20;
-		
-		if(a == b ) {
+		int b = 20;
+
+		if (a == b) {
 			System.out.println("They Are equal");
 		} else {
 			System.out.println("Not equal");
 		}
 
-}
-     //adding my name
-    public void print () {
-	System.out.println("lULU");
-	System.out.println("Sdet");
->>>>>>> branch 'master' of https://github.com/NTBatch2/CloneRepo.git
+	}
+
+	// adding my name
+	public void print() {
+		System.out.println("lULU");
+		System.out.println("Sdet");
 	}
 //	System.out.println("SDET Class");
-	//Stash Practice
+	// Stash Practice
 }
 //Robin
 //This is a line of code 
