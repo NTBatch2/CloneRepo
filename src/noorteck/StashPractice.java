26f42f05ed56129b38ad04f29c7f09bcf6909c34
saras <ag@gmail.com>
2020-05-16 14:57:08 -0400

    Sara

--- src/noorteck/StashPractice.java
+++ src/noorteck/StashPractice.java
@@ -7,12 +7,28 @@ public class StashPractice {
 
 	public static void main(String[] args) {
 
-	}
+		//stash....
 
+		int a = 10;
+		int b= 20;
+		
+		if(a == b ) {
+			System.out.println("They Are equal");
+		} else {
+			System.out.println("Not equal");
+		}
 
-	public void print() {
-		System.out.println("lULU");
-		System.out.println("Sdet");
-	}
 
 }
+     //adding my name
+    public void print () {
+	System.out.println("lULU");
+	System.out.println("Sdet");
+
+	}
+//	System.out.println("SDET Class");
+	//Stash Practice
+}
+//Robin
+//This is a line of code 
+//Again....................................................
