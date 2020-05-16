618d65f2c13dac73e2aaf060563009c5b1cef4f4
noort <noort@10.1.10.35>
2020-05-16 14:19:08 -0400

    added equal condition

--- src/noorteck/StashPractice.java
+++ src/noorteck/StashPractice.java
@@ -7,6 +7,14 @@ public class StashPractice {
 	
 	public static void main(String[] args) {
 		//stash....
+		int a = 10;
+		int b= 20;
+		
+		if(a == b ) {
+			System.out.println("They Are equal");
+		} else {
+			System.out.println("Not equal");
+		}
 	}
 	//Stash Practice
 }
