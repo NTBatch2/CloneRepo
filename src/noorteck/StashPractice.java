d9ad3536522ab5eb1edbe6d2c5c2a00eeff49270
NTBatch2 <65236389+NTBatch2@users.noreply.github.com>
2020-05-16 14:15:24 -0400

    added getName()method

--- src/noorteck/StashPractice.java
+++ src/noorteck/StashPractice.java
@@ -1,6 +1,10 @@
 package noorteck;
 
 public class StashPractice {
+	public static void getName(){
+		System.out.println("GithUb Commit");
+	}
+	
 	public static void main(String[] args) {
 		//stash....
 	}
