4d3e39f7c4d4a1791a200b1d94d4def18fd29812
NTBatch2 <65236389+NTBatch2@users.noreply.github.com>
2020-05-16 13:08:50 -0400

    Added more print statement in demo class

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -3,6 +3,10 @@ package noorteck;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("More GitHub....");
+		
+		System.out.println("From GitHub....");
+		System.out.println("From GitHub....");
+		System.out.println("From GitHub....");
 	}
 	
 	public void display() {
