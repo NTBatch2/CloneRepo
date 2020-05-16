94f50ab7de1f0c0ed85719a5f8690ee5ce44d5d3
noort <noort@10.1.10.190>
2020-05-15 22:43:24 -0400

    Added new method in Demo class

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -3,7 +3,9 @@ package noorteck;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("More GitHub....");
-		
-		Sytem.out.println("This line added from GitHub");
+	}
+	
+	public void display() {
+		System.out.println("This method for pull example");
 	}
 }
