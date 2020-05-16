6a1dcb2a9f6bd5897fba25258c2b8d7979c6716a
noort <noort@10.1.10.35>
2020-05-16 14:42:16 -0400

    fixed broken code

--- src/noorteck/StashPractice.java
+++ src/noorteck/StashPractice.java
@@ -7,25 +7,12 @@ public class StashPractice {
 
 	public static void main(String[] args) {
 
-		int a = 10;
-		int b = 20;
-
-		if (a == b) {
-			System.out.println("They Are equal");
-		} else {
-			System.out.println("Not equal");
-		}
-
 	}
 
-	// adding my name
+
 	public void print() {
 		System.out.println("lULU");
 		System.out.println("Sdet");
 	}
-//	System.out.println("SDET Class");
-	// Stash Practice
+
 }
-//Robin
-//This is a line of code 
-//Again....................................................
