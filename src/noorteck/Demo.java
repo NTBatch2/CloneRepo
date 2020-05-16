41180654f55f582dfe4342dc228c1822f96b555a
NTBatch2 <65236389+NTBatch2@users.noreply.github.com>
2020-05-15 20:14:49 -0400

    Added println statement

--- src/noorteck/Demo.java
+++ src/noorteck/Demo.java
@@ -3,5 +3,7 @@ package noorteck;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("More GitHub....");
+		
+		Sytem.out.println("This line added from GitHub");
 	}
 }
