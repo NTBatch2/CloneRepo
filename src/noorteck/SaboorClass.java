43e3289180f6afeb43af4cef155141e27bf6fc75
Abdul Azizi <saboorazizi.82@gmail.com>
2020-05-17 09:51:12 -0700

    My New Class!!

---
+++ src/noorteck/SaboorClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package noorteck;
+
+/**
+ * @author SABOOR
+ *
+ */
+public class SaboorClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My Commits");
+	}
+
+}
